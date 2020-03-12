30a501122b73e970e6051b7af3a3f9ec97044ffe
Alex <alex@kollectin.com>
2020-03-12 11:07:34 +0800

    init swift project

---
+++ IMC/IMC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by 陳駿逸 on 2020/3/12.
+//  Copyright © 2020 陳駿逸. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
