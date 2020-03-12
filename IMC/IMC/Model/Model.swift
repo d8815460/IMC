bb79e2b23ed750f1eb177ce1170b9b099d8341e0
Alex <alex@kollectin.com>
2020-03-12 13:22:24 +0800

    set architecture of MVVM

---
+++ IMC/IMC/Model/Model.swift
@@ -0,0 +1,12 @@
+//
+//  Model.swift
+//  IMC
+//
+//  Created by 陳駿逸 on 2020/3/12.
+//  Copyright © 2020 陳駿逸. All rights reserved.
+//
+
+
+struct UserDataModel {
+    
+}
---
+++ IMC/IMC/View Model/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  IMC
+//
+//  Created by 陳駿逸 on 2020/3/12.
+//  Copyright © 2020 陳駿逸. All rights reserved.
+//
+
+import UIKit
+
+class ViewModel {
+
+}
